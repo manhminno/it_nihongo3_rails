0ffb374b38b4684d8b5da977fb36b58cb72718e0
manhminno <manh6264@gmail.com>
2020-11-25 17:21:50 +0000

    Add image on book

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -109,4 +109,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  
+  #upload image from google
+  config.active_storage.service = :google
 end
